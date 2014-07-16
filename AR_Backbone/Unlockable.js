0371590879f0c0d4657c6a0ada7caaac86679138
Daniel Shumway <shumway.danny@gmail.com>
2014-07-16 15:13:17 -0400

    Testing some stuff.

--- AR_Backbone/Unlockable.js
+++ AR_Backbone/Unlockable.js
@@ -8,8 +8,8 @@ function Unlockable(template, destination, id){
 	//build the dom.
 	//To make things easier for devs, we copy the node from a predefined point.
 	var element = template.cloneNode(true); //True means that it's a deep copy.  It copies sub-nodes as well.
-	destination.appendChild(element); //And add it to the list.
-
+	//destination.appendChild(element); //And add it to the list.  This is something the parent should handle.
+   
 
 	//Add in the element that we just made.
 	var item = DomWrapper(0, 0, 0, 0, element);
@@ -22,10 +22,9 @@ function Unlockable(template, destination, id){
 	//And just capture events at the module and handle them as events.
 	//It's also, honestly, more in the spirit of a modular approach, since we don't need to worry about
 	//passing mouse clicks through the entire application all the time.  It just works.
-	element.onclick = function(){
-		if(toReturn.unlocked) { base.handleEvent("selected", {"unlocked":true}); }
-		else { base.handleEvent("selected", {"unlocked":false}); }
-	}
+	element.addEventListener("click", function(e){
+		base.handleEvent("selected", e);
+	}, true);
 	//And add the event.
 	base.addEvent("selected", function(_clipBoard){
 		//Navigate to the proper place.
