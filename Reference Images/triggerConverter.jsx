8abfe41704b850c1507036577a898d322da10f6d
Daniel Shumway <shumway.danny@gmail.com>
2014-08-29 12:33:57 -0400

    First of resized triggers. (Maybe?) Fixed an error with text placement on small screens, still needs to be checked.
    Signed-off-by: Daniel Shumway <shumway.danny@gmail.com>

---
+++ Reference Images/triggerConverter.jsx
@@ -0,0 +1,21 @@
+﻿var directory = (new File($.fileName)).parent;
+for (var i = 0; i < 1023; i++){
+    //Open and resize up
+    var docRef = app.open(new File(directory + "/triggers/" + i + ".png"));
+    docRef.resizeImage("700px", "700px", 100, ResampleMethod.NEARESTNEIGHBOR);
+    
+    
+    //Save
+    var opts = new ExportOptionsSaveForWeb();
+
+    opts.PNG8 = false;
+    opts.transparency = false;
+    opts.interlaced = false;
+    opts.quality = 100;
+    opts.includeProfile = false;
+    opts.format = SaveDocumentType.PNG; // Document Type
+    docRef.exportDocument( new File( directory + "/resizedTriggers/" + i + ".png"),  ExportType.SAVEFORWEB, opts ); 
+
+    //Close (free up space, etc...)
+    docRef.close(SaveOptions.DONOTSAVECHANGES);
+ }
